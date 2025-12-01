909af62d272724cc95fb9a313175b86ed54cb46e
Emiliano Agustin Pintos <30700594+emilianoagustin@users.noreply.github.com>
2025-12-01 11:15:51 -0300

    Dev/fix 04 (#8)
    * added cors options to preflight
    
    * added a plain string in cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import authRoutes from "./src/routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 5000;
-const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
+const allowedOrigin = "https://front-proyecto-final.vercel.app";
 const corsOptions = {
   origin: allowedOrigin,
   methods: "GET,POST,PUT,DELETE",
@@ -26,5 +26,5 @@ app.use("/api/auth", authRoutes);
 app.use(notFound);
 
 app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`),
 );
